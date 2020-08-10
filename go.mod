7e6e3754df0dedaa6e5be68e60756b9648db9410
Paddy Gallagher <paddy.gallagher@gmail.com>
2020-08-10 12:00:22 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paddycakes/imagine
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", HelloPubSub)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// PubSubMessage is the payload of a Pub/Sub event.
+type PubSubMessage struct {
+	Message struct {
+		Data []byte `json:"data,omitempty"`
+		ID   string `json:"id"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
+}
+
+// HelloPubSub receives and processes a Pub/Sub push message.
+func HelloPubSub(w http.ResponseWriter, r *http.Request) {
+	var m PubSubMessage
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("ioutil.ReadAll: %v", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	if err := json.Unmarshal(body, &m); err != nil {
+		log.Printf("json.Unmarshal: %v", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	greeting := os.Getenv("TARGET")
+	if greeting == "" {
+		greeting = "Hello"
+	}
+
+	name := string(m.Message.Data)
+	if name == "" {
+		name = "Go Lovers"
+	}
+
+	fmt.Fprintf(w, "%s %s!\n", greeting, name)
+	log.Printf("go-crazy: %s %s!\n", greeting, name)
+}
